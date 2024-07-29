2ef31f7a473ab22949f0a84b8799d9a4b2b5cb3a
Arfat <91958640+arfatwadekar@users.noreply.github.com>
2024-07-29 14:43:56 +0530

    Add files via upload

--- assests/Js/main.js
+++ assests/Js/main.js
@@ -12,11 +12,11 @@ window.onscroll = () => {
 };
 
 //   scroll up
-const toTop = document.querySelector(".to_top");
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
-  }
-});
+// const toTop = document.querySelector(".to_top");
+// window.addEventListener("scroll", () => {
+// if (window.pageYOffset > 100) {
+// toTop.classList.add("active");
+// } else {
+// toTop.classList.remove("active");
+// }
+// });
